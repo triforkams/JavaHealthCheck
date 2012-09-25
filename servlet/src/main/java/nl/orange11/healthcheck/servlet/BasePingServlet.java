5eed354e134f4221fc2d1254687d4915adc72203
jettro <jettro@gridshore.nl>
2012-09-25 09:20:42 +0200

    Add caching headers to the response of the ping

--- servlet/src/main/java/nl/orange11/healthcheck/servlet/BasePingServlet.java
+++ servlet/src/main/java/nl/orange11/healthcheck/servlet/BasePingServlet.java
@@ -93,6 +93,12 @@ public abstract class BasePingServlet extends HttpServlet {
 
         int responseCode = obtainStatusOfResponse(pingResult);
         res.setStatus(responseCode);
+
+        // Disable Caching
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        res.setDateHeader("Expires", 0); // Proxies.
+
         if ("application/json".equals(req.getHeader("Content-Type")) || "json".equals(req.getParameter("type"))) {
             writeJsonResponse(res, pingResult);
         } else {
